70d8a4c44321d1b80fea5bf90d06126c39ae4074
yousif-saif <mrdento82@gmail.com>
2023-09-07 15:01:18 +0300

    making frontend for upload

--- app.js
+++ app.js
@@ -36,32 +36,38 @@ app.get("/watch", (req, res) => {
 
 })
 app.get("/video", (req, res) => {
-    const range = req.headers.range;
+    const range = req.headers.range
     if (!range) {
-      res.status(400).send("Requires Range header");
+      res.status(400).send("Requires Range header")
     }
     
     const id = req.query.id
 
-    const videoPath = __dirname + "/public/videos/" + id + ".mp4"
-    const videoSize = fs.statSync(videoPath).size;
+    // const videoPath = __dirname + "/public/videos/" + id + ".mp4"
+    const videoPath = __dirname + "/public/videos/new_vid.mp4"
+    const videoSize = fs.statSync(videoPath).size
 
-    const CHUNK_SIZE = 10 ** 6; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const CHUNK_SIZE = 10 ** 6 // 1MB
+    const start = Number(range.replace(/\D/g, ""))
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
   
     // Create headers
-    const contentLength = end - start + 1;
+    const contentLength = end - start + 1
     const headers = {
       "Content-Range": `bytes ${start}-${end}/${videoSize}`,
       "Accept-Ranges": "bytes",
       "Content-Length": contentLength,
       "Content-Type": "video/mp4",
-    };
+    }
   
-    res.writeHead(206, headers);
+    res.writeHead(206, headers)
   
-    const videoStream = fs.createReadStream(videoPath, { start, end });
+    const videoStream = fs.createReadStream(videoPath, { start, end })
   
-    videoStream.pipe(res);
-});
+    videoStream.pipe(res)
+})
+
+
+app.get("/upload", (req, res) => {
+    res.render("upload.ejs")
+})
